eb54de670ac5090a4b8fd8bf8cb0cc6888adf753
rico_guo <1476262983@qq.com>
2023-06-15 01:50:35 +0800

    add 实现注册功能

--- config/routes.ts
+++ config/routes.ts
@@ -1,16 +1,22 @@
 export default [
-  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
-  { path: '/welcome', icon: 'smile', component: './Welcome' },
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {path: '/user/login', component: './User/Login'},
+      {path: '/user/register', component: './User/Register'}]
+  },
+  {path: '/welcome', icon: 'smile', component: './Welcome'},
   {
     path: '/admin',
     icon: 'crown',
     access: 'canAdmin',
     routes: [
-      { path: '/admin', redirect: '/admin/sub-page' },
-      { path: '/admin/sub-page', component: './Admin' },
+      {path: '/admin', redirect: '/admin/sub-page'},
+      {path: '/admin/sub-page', component: './Admin'},
     ],
   },
-  { icon: 'table', path: '/list', component: './TableList' },
-  { path: '/', redirect: '/welcome' },
-  { path: '*', layout: false, component: './404' },
+  {icon: 'table', path: '/list', component: './TableList'},
+  {path: '/', redirect: '/welcome'},
+  {path: '*', layout: false, component: './404'},
 ];
---
+++ src/pages/User/Register/index.tsx
@@ -0,0 +1,191 @@
+import Footer from '@/components/Footer';
+
+import {LockOutlined, UserOutlined,} from '@ant-design/icons';
+import {LoginForm, ProFormText,} from '@ant-design/pro-components';
+import {useEmotionCss} from '@ant-design/use-emotion-css';
+import {Helmet, history, useModel} from '@umijs/max';
+import {message, Tabs} from 'antd';
+import React, {useState} from 'react';
+import {flushSync} from 'react-dom';
+import Settings from '../../../../config/defaultSettings';
+import {getCurrentUserUsingGET, userRegisterUsingPOST} from "@/services/rico/userController";
+import {Link} from "@@/exports";
+
+const Login: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const {setInitialState} = useModel('@@initialState');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+  const fetchUserInfo = async () => {
+    const userInfo = await getCurrentUserUsingGET();
+    if (userInfo) {
+      flushSync(() => {
+        setInitialState((s) => ({
+          ...s,
+          currentUser: userInfo,
+        }));
+      });
+    }
+  };
+  const handleSubmit = async (values: API.UserRegisterRequest) => {
+    try {
+      // 登录
+      const res = await userRegisterUsingPOST(values);
+      if (res.code === 0) {
+        const defaultLoginSuccessMessage = '注册成功！';
+        message.success(defaultLoginSuccessMessage);
+        await fetchUserInfo();
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/');
+        return;
+      } else {
+        message.error(res.message);
+      }
+
+    } catch (error) {
+      const defaultLoginFailureMessage = '注册失败，请重试！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg"/>}
+          title="车享"
+          subTitle={'车享 开启您的新生活'}
+          initialValues={{
+            autoLogin: true,
+          }}
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserRegisterRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '注册',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined/>,
+                }}
+                placeholder={'请输入用户名'}
+                rules={[
+                  {
+                    required: true,
+                    message: '用户名是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText
+                name="userPhone"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined/>,
+                }}
+                placeholder={'请输入手机号'}
+                rules={[
+                  {
+                    required: true,
+                    message: '手机号不可以为空',
+                  },
+                  {
+                    len: 11,
+                    message: '请输入正确的手机号',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined/>,
+                }}
+                placeholder={'请输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码不可以为空',
+                  },
+                  {
+                    min: 8,
+                    message: '密码至少是8位！',
+                  }
+                ]}
+              />
+              <ProFormText.Password
+                name="userCheckPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined/>,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '确认密码不可以为空',
+                  },
+                  {
+                    min: 8,
+                    message: '确认密码至少是8位！',
+                  }
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Link
+              style={{
+                float: 'right',
+              }}
+              to={'/user/login'}
+            >
+              返回登录界面
+            </Link>
+          </div>
+        </LoginForm>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+export default Login;
